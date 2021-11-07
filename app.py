fbc4238d8af3f322c280d9e57260b56fcb0b1708
Efstratios Pahis <efstratios.pahis@tum.de>
2021-11-07 23:42:49 +0100

    New version

--- app.py
+++ app.py
@@ -15,19 +15,19 @@ import Flask.Endpoints.DataHealthAPI as api_dh
 import Flask.Endpoints.ArchitectureViewAPI as api_av
 
 
-BRapp = Flask(__name__)
+app = Flask(__name__)
 CORS(BRapp)
 
 @BRapp.route("/")
 def helloWorld():
     return "<html>Hello says Project Athena</html>"
 
-BRapp.register_blueprint(api_dm.blueprint)
-BRapp.register_blueprint(api_um.blueprint)
-BRapp.register_blueprint(api_dc.blueprint)
-BRapp.register_blueprint(api_da.blueprint)
-BRapp.register_blueprint(api_dh.blueprint)
-BRapp.register_blueprint(api_av.blueprint)
+app.register_blueprint(api_dm.blueprint)
+app.register_blueprint(api_um.blueprint)
+app.register_blueprint(api_dc.blueprint)
+app.register_blueprint(api_da.blueprint)
+app.register_blueprint(api_dh.blueprint)
+app.register_blueprint(api_av.blueprint)
     
 
 
